9f3e45275c8383024733a12901bfb5e2d9fad22b
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-06-28 18:10:09 +0000

    subo logica de registrar usuario

---
+++ trunk/quegolazo-code/AccesoADatos/DAOUsuario.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AccesoADatos
+{
+  public  class DAOUsuario
+    {
+        public string cadenaDeConexion = System.Configuration.ConfigurationManager.ConnectionStrings["localhost"].ConnectionString;
+
+        public DAOUsuario()
+        {
+
+        }
+
+
+
+        public void registrarUsuario(Usuario usuarioNuevo)
+        {
+            SqlConnection con = new SqlConnection(cadenaDeConexion);
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                    cmd.Connection = con;
+                }
+
+                string sql = @"INSERT INTO Usuarios (nombre, apellido, email, contrasenia, codigo, idTipoUsuario)
+                              VALUES (@nombre, @apellido, @email, @contrasenia, @codigo, @idTipoUsuario)";
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add(new SqlParameter("@nombre", usuarioNuevo.nombre));
+                cmd.Parameters.Add(new SqlParameter("@apellido", usuarioNuevo.apellido));
+                cmd.Parameters.Add(new SqlParameter("@email", usuarioNuevo.email));
+                cmd.Parameters.Add(new SqlParameter("@contrasenia", usuarioNuevo.contrasenia));
+                cmd.Parameters.Add(new SqlParameter("@codigo", usuarioNuevo.codigo));
+                cmd.Parameters.Add(new SqlParameter("@idTipoUsuario", usuarioNuevo.tipoUsuario.idTipoUsuario));
+
+                cmd.CommandText = sql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo registrar el Usuario: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        public Usuario buscarUsuarioPorEmail(string email)
+        {
+            SqlConnection con = new SqlConnection(cadenaDeConexion);
+            SqlCommand cmd = new SqlCommand();
+
+
+            Usuario respuesta = null;
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                    cmd.Connection = con;
+                }
+
+                string sql = @"SELECT *
+                                FROM Usuarios
+                                WHERE email = @email";
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.CommandText = sql;
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                DAOTipoUsuario gestorTipoUsuario = new DAOTipoUsuario();
+
+                while (dr.Read())
+                {
+                    respuesta = new Usuario();
+                    respuesta.idUsuario = Int32.Parse(dr["idUsuario"].ToString());
+                    respuesta.nombre = dr["nombre"].ToString();
+                    respuesta.apellido = dr["apellido"].ToString();
+                    respuesta.email = dr["email"].ToString();
+
+                    respuesta.contrasenia = dr["contrasenia"].ToString();
+
+                    if (dr["esActivo"].ToString().Equals("1"))
+                        respuesta.esActivo = true;
+                    else
+                        respuesta.esActivo = false;
+
+                    respuesta.codigo = dr["codigo"].ToString();
+                    respuesta.tipoUsuario = gestorTipoUsuario.obtenerTipoUsuarioPorId(Int32.Parse(dr["idTipoUsuario"].ToString()));
+
+                }
+                return respuesta;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error al intentar recuperar el campeonato: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+
+
+/// <summary>
+/// Metodo para activar cuenta
+/// autor: Flor
+/// </summary>
+/// <param name="IdUsuario"></param>
+        public void ActivarCuenta(int IdUsuario)
+        {
+           
+            SqlConnection con = new SqlConnection(cadenaDeConexion);
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                    cmd.Connection = con;
+                }
+
+                if (IdUsuario != null)
+                {   
+                    string sql = "UPDATE Usuarios SET EsActivo=1 WHERE IdUsuario=@UserID";
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@UserID", IdUsuario);
+                    cmd.CommandText = sql;
+
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se pudo activar su usuario. Comuníquese con nuestro soporte técnico.");
+            }
+            finally
+            {
+                con.Close();
+
+            }
+            
+        }
+
+
+    }
+}
---
+++ trunk/quegolazo-code/Logica/GestorUsuario.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Security.Cryptography;
+using AccesoADatos;
+
+namespace Logica
+{
+   public class GestorUsuario
+    {
+        /// <summary>
+        /// autor=Flor
+        /// Método para tomar los datos de la pantalla y crear la entidad Usuario
+        /// </summary>
+        /// <param name="apellido"></param>
+        /// <param name="nombre"></param>
+        /// <param name="mail"></param>
+        /// <param name="telefono"></param>
+        /// <param name="contrasenia"></param>
+        /// <returns>Usuario</returns>
+        public void registrarUsuario(string apellido, string nombre, string mail, string contrasenia)
+        {
+            try
+            {
+                Usuario u = new Usuario
+                {
+                    apellido = apellido,
+                    nombre = nombre,
+                    email = mail,
+                    contrasenia = encriptarContrasenia(contrasenia),
+                    codigo = "123456789",//Falta metodo para generar codigo único
+                    tipoUsuario = new TipoUsuario { idTipoUsuario = 1, nombre = "Administrador" },
+                };
+
+                DAOUsuario gestorBD = new DAOUsuario();
+                gestorBD.registrarUsuario(u);//guarda en la BD
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            ;
+        }
+
+        /// <summary>
+        /// Método para encriptar clave
+        /// autor=Flor
+        /// </summary>
+        /// <param name="claveSinencriptar"></param>
+        /// <returns></returns>
+        private string encriptarContrasenia(string claveSinencriptar)
+        {
+
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(claveSinencriptar);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+
+
+        /// <summary>
+        /// Método para llamar al activarUsuario
+        /// autor=Flor
+        /// </summary>
+        /// <param name="claveSinencriptar"></param>
+        /// <returns></returns>
+        public void activarUsuario(int IdUsuario)
+        {
+            DAOUsuario gestorBD = new DAOUsuario();
+            gestorBD.ActivarCuenta(IdUsuario);
+        }
+         
+    }
+}
