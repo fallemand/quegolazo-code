3e1aa3fa869358c75852be3b4a75bb37ee03632f
facualle <facualle@hotmail.com>
2014-07-02 16:48:50 +0000

    -Se modificó menú superior -Se agregó cerrar sesión -Se agrego GestorExtra

---
+++ quegolazo-code/Utils/GestorExtra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public class GestorExtra
+    {
+        public static string truncarString(string texto, int cantCaracteres)
+        {
+            if (string.IsNullOrEmpty(texto))
+            {
+                return texto;
+            }
+            if (texto.Length > cantCaracteres)
+            {
+                return texto.Substring(0, cantCaracteres) + "..";
+            }
+            else
+            {
+                return texto;
+            }
+        }
+    }
+}
