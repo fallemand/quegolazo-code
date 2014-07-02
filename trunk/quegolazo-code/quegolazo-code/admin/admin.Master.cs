79f8c76529af7d75dbaa41e35ef97c61e5f5582b
facualle@hotmail.com <facualle@hotmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-07-02 16:48:50 +0000

    -Se modificó menú superior -Se agregó cerrar sesión -Se agrego GestorExtra

--- trunk/quegolazo-code/quegolazo-code/admin/admin.Master.cs
+++ trunk/quegolazo-code/quegolazo-code/admin/admin.Master.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entidades;
 
 namespace quegolazo_code.admin
 {
@@ -11,7 +13,23 @@ namespace quegolazo_code.admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                if (Session["usuario"] != null)
+                {
+                    Usuario usuario = (Usuario)Session["usuario"];
+                    Literal litNombre = (Literal)lvNavBar.FindControl("LitNombre");
+                    litNombre.Text = usuario.nombre;
+                }
+            }
+        }
 
+        protected void hlCerrarSesion_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            Response.Redirect("login.aspx");
         }
     }
 }
