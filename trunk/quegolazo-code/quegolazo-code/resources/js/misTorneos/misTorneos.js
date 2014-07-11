0789451eda627fb5b442cf7ed6d2378a5b0917c5
antonioherrera990@gmail.com <antonioherrera990@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-07-11 23:37:56 +0000

    Avance en modificacion de torneo, por ahora todo el codigo esta en el archivo mistorneos.js falta programar bien la parte del servidor que solo agregue el metodo al DAO y al gestor torneo

--- trunk/quegolazo-code/quegolazo-code/resources/js/misTorneos/misTorneos.js
+++ trunk/quegolazo-code/quegolazo-code/resources/js/misTorneos/misTorneos.js
@@ -1,9 +1,21 @@
-﻿function modificarTorneo(idTorneo, url) {    
-        $('#modalTorneoLabel').text("Modifcar Torneo");     
-        $('#ContentAdmin_txtUrlTorneo').text(url);
-        $('#ContentAdmin_txtUrlTorneo').prop("disabled", true);        
+﻿function modificarTorneo(idTorneo, url, nombre, descripcion) {    
+        $('#modalTorneoLabel').text("Modificar Torneo");
+        $('#ContentAdmin_txtUrlTorneo').val(url);
+        $('#ContentAdmin_txtDescripcion').val(descripcion);
+        $('#ContentAdmin_txtUrlTorneo').prop("disabled", true);
+        $('#ContentAdmin_txtNombreTorneo').val(nombre);
         $('#ContentAdmin_btnResgitrarTorneo').hide();
-        $('#logoTorneoPreview').css('background-image', 'url(' + $('#img'+idTorneo).attr('src') + ')');
-        $('#logoTorneoPreview').addClass('fileinput-preview thumbnail');
+        $('#ContentAdmin_btnModificarTorneo').show();
+        $('#imagen-preview').attr('src', $('#img' + idTorneo).attr('src')); 
+}
+function crearTorneo() {
+    $('#modalTorneoLabel').text("Registrar Nuevo Torneo");
+    $('#ContentAdmin_txtUrlTorneo').val("");
+    $('#ContentAdmin_txtDescripcion').val("");
+    $('#ContentAdmin_txtUrlTorneo').prop("disabled", false);
+    $('#ContentAdmin_txtNombreTorneo').val("");
+    $('#ContentAdmin_btnResgitrarTorneo').show();
+    $('#ContentAdmin_btnModificarTorneo').hide();
+    $('#imagen-preview').attr('src', "../resources/img/theme/logo-default.png");
+}
 
-    }
