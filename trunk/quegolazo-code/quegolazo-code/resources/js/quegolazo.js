c6ad11b5ebf887147e85bd43d16bd76f815f8b4e
facualle@hotmail.com <facualle@hotmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-07-29 20:20:02 +0000

    -Se agrego quegolazo.js

---
+++ trunk/quegolazo-code/quegolazo-code/resources/js/quegolazo.js
@@ -0,0 +1,50 @@
+﻿function modificarTorneo(idTorneo, url, nombre, descripcion) {
+    $('#modalTorneoLabel').text("Modificar Torneo");
+    $('#ContentAdmin_txtUrlTorneo').val(url);
+    $('#ContentAdmin_txtDescripcion').val(descripcion);
+    $('#ContentAdmin_txtUrlTorneo').prop("disabled", true);
+    $('#ContentAdmin_txtNombreTorneo').val(nombre);
+    $('#ContentAdmin_btnResgitrarTorneo').hide();
+    $('#ContentAdmin_btnModificarTorneo').show();
+    $('#imagen-preview').attr('src', $('#img' + idTorneo).attr('src'));
+};
+function crearTorneo() {
+    $('#modalTorneoLabel').text("Registrar Nuevo Torneo");
+    $('#ContentAdmin_txtUrlTorneo').val("");
+    $('#ContentAdmin_txtDescripcion').val("");
+    $('#ContentAdmin_txtUrlTorneo').prop("disabled", false);
+    $('#ContentAdmin_txtNombreTorneo').val("");
+    $('#ContentAdmin_btnResgitrarTorneo').show();
+    $('#ContentAdmin_btnModificarTorneo').hide();
+    $('#imagen-preview').attr('src', "../resources/img/theme/logo-default.png");
+};
+function closeModal(idModal) {
+    $('#' + idModal).modal('hide');
+};
+function openModal(idModal) {
+    $('#' + idModal).modal('show');
+};
+function togglePanel(panelId) { $('#' + panelId + '').collapse('toggle'); };
+function activaTab(nombreGrupo, nombreTab) {
+    $('#' + nombreGrupo + ' a[href="#' + nombreTab + '"]').tab('show');
+};
+function addClass(id, clase) {
+    $('#' + id).addClass(clase);
+};
+function removeClass(id, clase) {
+    $('#' + id).removeClass(clase);
+};
+
+// mis-torneos.aspx
+$('.fileinput').on('change.bs.fileinput', function () {
+    $('.fileinput-preview').css('background-image', 'none');
+});
+$('#modalTorneo').on('hidden.bs.modal', function () {
+    $('.modal-body').find('input[type=text], input[type=password], input[type=number], input[type=email], textarea').val('');
+    $('.modal-body').find('div').removeClass('has-success has-error');
+});
+$('#modalEdicion').on('hidden.bs.modal', function () {
+    $('.modal-body').find('input[type=text], input[type=password], input[type=number], input[type=email], textarea').val('');
+    $('.modal-body').find('div').removeClass('has-success has-error');
+});
+
