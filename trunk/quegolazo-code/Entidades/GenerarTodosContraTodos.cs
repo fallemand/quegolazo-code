05d63b0f38a2360211bffb3e9a99bb43966cbdbc
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-08-18 19:36:41 +0000

    ahora si compila

---
+++ trunk/quegolazo-code/Entidades/GenerarTodosContraTodos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class GenerarTodosContraTodos: IGenerarFixture
+    {
+        public List<Fecha> generarFixture(List<Equipo> equiposParticipantes)
+        {
+            //List<Fecha> listajornadas = new List<Fecha>();
+
+            //for (int y = 1; y < equiposParticipantes.Count; y++) // for para Fechas
+            //{
+            //    Fecha fecha = new Fecha();
+
+            //    fecha.idFecha = (y);
+
+            //    for (int x = 1; x <= equiposParticipantes.Count / 2; y++)               // for para Partidos
+            //    {
+            //        PartidoComun partido = new PartidoComun();
+
+            //        // Aquí como hago para añadir los enfrentamientos de acuerdo a las reglas que puse previamente?
+
+            //        jornada.enfrentamientos.Add(enfrentamiento);
+            //    }
+
+            //    listajornadas.Add(jornada);
+            //}
+        }
+    }
+}
---
+++ trunk/quegolazo-code/Logica/GestorFase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoADatos;
+using Utils;
+
+namespace Logica
+{
+  public  class GestorFase
+    {
+      public List<Equipo> equipos { get; set; }
+      public Fase fase { get; set; }
+
+
+      public List<Fecha> generarFixture(List<Equipo> EquiposDelGrupo, string tipoFixture)
+      {
+          return new List<Fecha>();
+      }
+
+      public void registrarFase( string tipoFixture)
+      {
+          fase= new Fase();
+          fase.tipoFixture.nombre = tipoFixture;
+          fase.grupos = new List<Grupo>();
+      }
+
+
+    }
+}
