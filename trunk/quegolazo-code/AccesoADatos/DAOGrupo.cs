408c0b9413583a0b10f8865ad9518e1d868fcdd0
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-08-18 22:41:10 +0000

    subo daos para registrar fase

---
+++ trunk/quegolazo-code/AccesoADatos/DAOGrupo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+namespace AccesoADatos
+{
+    public class DAOGrupo
+    {
+        public string cadenaDeConexion = System.Configuration.ConfigurationManager.ConnectionStrings["localhost"].ConnectionString;
+        /// <summary>
+        /// Registrar Grupo, es parte de una transaccion al registrar Fase.
+        /// autor: Flor Rojas
+        /// </summary>
+        /// <param name="delegado">Nuevo delegado a registrar</param>
+        /// <param name="con">La conexion abierta de la transaccion.</param>
+        /// <param name="trans">La transaccion de registro de equipo</param>
+        /// <returns>Id del delegado registrado</returns>
+        public void registrarGrupos(Fase fase, SqlConnection con, SqlTransaction trans)
+        {
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.Transaction = trans;
+                foreach (Grupo g in fase.grupos)
+                {
+                    string sql = @"INSERT INTO Grupos (idGrupo,idFase,idEdicion)
+                                VALUES (@idGrupo,@idFase,@idEdicion,@nombre) 
+                                SELECT SCOPE_IDENTITY()";
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@idGrupo", g.idGrupo);
+                    cmd.Parameters.AddWithValue("@idFase", fase.idFase);
+                    cmd.Parameters.AddWithValue("@idEdicion", fase.idEdicion);
+                    cmd.Parameters.AddWithValue("@nombre", g.nombre);
+                    cmd.CommandText = sql;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo registrar el grupo:" + ex.Message);
+            }
+        }
+    }
+}
---
+++ trunk/quegolazo-code/AccesoADatos/DAOPartido.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+
+
+namespace AccesoADatos
+{
+   public class DAOPartido
+    {
+        public string cadenaDeConexion = System.Configuration.ConfigurationManager.ConnectionStrings["localhost"].ConnectionString;
+        /// <summary>
+        /// Registrar Partido de las Fechas de un Partido, es parte de una transaccion al registrar Fase.
+        /// autor: Flor Rojas
+        /// </summary>
+        /// <param name="fase">Fase que se esta grabando</param>
+        /// <param name="con">La conexion abierta de la transaccion.</param>
+        /// <param name="trans">La transaccion de registro de equipo</param>
+        /// <returns>Id del delegado registrado</returns>
+        public void registrarPartidos(Fase fase, SqlConnection con, SqlTransaction trans)
+        {
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.Transaction = trans;
+                foreach (Grupo g in fase.grupos)
+                {
+                    foreach (Fecha f in g.fixture)
+                    {
+                         foreach (IPartido p in f.partidos)
+                    {
+                        string sql = @"INSERT INTO Partidos (idPartido,idFecha,idGrupo,idFase,idEdicion,idEquipoLocal,idEquipoVisitante,fecha)
+                                     VALUES (@idPartido,@idFecha,@idGrupo,@idFase,@idEdicion,@idEquipoLocal,@idEquipoVisitante,@fecha)";
+                        cmd.Parameters.Clear();
+                        cmd.Parameters.AddWithValue("@idPartido", p.idPartido);
+                        cmd.Parameters.AddWithValue("@idFecha", f.idFecha);
+                        cmd.Parameters.AddWithValue("@idGrupo", g.idGrupo);
+                        cmd.Parameters.AddWithValue("@idFase", fase.idFase);
+                        cmd.Parameters.AddWithValue("@idEdicion", fase.idEdicion);
+                        cmd.Parameters.AddWithValue("@idEquipoLocal", p.local.idEquipo);
+                        cmd.Parameters.AddWithValue("@idEquipoVisitante", p.visita.idEquipo);
+                        cmd.Parameters.AddWithValue("@fecha", p.fecha + " "+p.hora);
+                        cmd.CommandText = sql;
+                        cmd.ExecuteNonQuery();
+                    }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo registrar el partido" + ex.Message);
+            }
+        }
+    }
+}
