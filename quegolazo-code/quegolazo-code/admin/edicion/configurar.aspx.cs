fd4b08a76e8f694fd43ec3769c8c84446fd00fb6
antonioherrera990 <antonioherrera990@gmail.com>
2014-08-19 19:29:40 +0000

    Funcionalidad agragada al formulario configuracion

--- quegolazo-code/quegolazo-code/admin/edicion/configurar.aspx.cs
+++ quegolazo-code/quegolazo-code/admin/edicion/configurar.aspx.cs
@@ -10,9 +10,36 @@ namespace quegolazo_code.admin.edicion
 {
     public partial class configurar : System.Web.UI.Page
     {
+        GestorEdicion gestorEdicion = new GestorEdicion();   
         protected void Page_Load(object sender, EventArgs e)
         {
-            GestorFase GestorFase = new GestorFase();
+            gestorEdicion = Sesion.getGestorEdicion();           
+        }
+        protected void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Preferencias Jugadores
+                gestorEdicion.edicion.preferencias.jugadores = rdJugadoresRegistroSi.Checked;
+                gestorEdicion.edicion.preferencias.golesJugadores = rdJugadoresGolesSi.Checked;
+                gestorEdicion.edicion.preferencias.tarjetasJugadores = rdJugadoresTarjetasSi.Checked;
+                gestorEdicion.edicion.preferencias.cambiosJugadores = rdJugadoresCambiosSi.Checked;
+                //Preferencias Árbitros
+                gestorEdicion.edicion.preferencias.arbitros = rdArbitrosSi.Checked;
+                gestorEdicion.edicion.preferencias.asignaArbitros = rdArbitrosPorPartidoSi.Checked;
+                gestorEdicion.edicion.preferencias.desempenioArbitros = rdArbitroDesempenioSi.Checked;
+                //Preferencias Sanciones
+                gestorEdicion.edicion.preferencias.sanciones = rdSancionesEquiposSi.Checked;
+                gestorEdicion.edicion.preferencias.sancionesJugadores = rdSancionesJugadoresSi.Checked;
+                //Preferencia Canchas
+                gestorEdicion.edicion.preferencias.canchaUnica = rdCanchasComplejos.Checked;
+                Response.Redirect("equipos.aspx"); 
+            }
+            catch (Exception ex)
+            {
+                litFracaso.Text = ex.Message;
+                panelFracaso.Visible = true;                
+            }
         }
     }
 }
