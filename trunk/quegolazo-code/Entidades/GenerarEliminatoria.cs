184f15c2603451037ad240ab4b47d4b1d89f8823
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-10-04 19:00:03 +0000

    Agrego nuevo formuladrio para ver ediciones

---
+++ trunk/quegolazo-code/Entidades/GenerarEliminatoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entidades
+{
+    class GenerarEliminatoria : IGenerarFixture
+    {
+
+        public List<Fecha> generarFixture(List<Equipo> equiposParticipantes)
+        {
+            List<Fecha> fechas = new List<Fecha>();
+            
+            return fechas;
+        }
+
+        public int getCantidadRondas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setCantidadRondas(int cantidadRondas)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
