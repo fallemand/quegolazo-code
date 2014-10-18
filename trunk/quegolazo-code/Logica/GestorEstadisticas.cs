f2169273ee71cab5cab11a26b6d499dfebec452d
florenciarojasamaya@gmail.com <florenciarojasamaya@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2014-10-18 14:50:27 +0000

    Agrego metodos de base de datos para las estadisticas

---
+++ trunk/quegolazo-code/Logica/GestorEstadisticas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoADatos;
+using Utils;
+using System.Data;
+
+namespace Logica
+{
+   public  class GestorEstadisticas
+    {
+       //DAOEstadisticas
+       Edicion edicion;
+       DAOEstadisticas DaoEstadisticas = new DAOEstadisticas();
+
+       public GestorEstadisticas()
+       {
+           edicion = Sesion.getEdicion();
+       }
+
+       public DataTable obtenerTablaPosiciones()
+       {
+           return DaoEstadisticas.obtenerTablaPosiciones(edicion.idEdicion);
+       }
+
+       public DataTable obtenerFixtureFecha(int idFecha)
+       {
+           return DaoEstadisticas.obtenerFixtureFecha(edicion.idEdicion, idFecha);
+       }
+
+       public DataTable obtenerFixtureUltimaFecha(int idEstadoFecha)//8 o 9
+       {
+           return DaoEstadisticas.obtenerFixtureUltimaFecha(edicion.idEdicion,idEstadoFecha);
+       }
+
+       public DataTable obtenerAvanceFecha(int idEstadoFecha)//8 o 9
+       {
+           return DaoEstadisticas.obtenerAvanceFecha(edicion.idEdicion,idEstadoFecha);
+       }
+
+       public DataTable obtenerAvanceEdicion()
+       {
+           return DaoEstadisticas.obtenerAvanceEdicion(edicion.idEdicion);
+       }
+
+    }
+}
