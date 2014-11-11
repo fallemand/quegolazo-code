4354d4388673af43d76f55d8835aacd5541e1641
florenciarojasamaya <florenciarojasamaya@gmail.com>
2014-11-11 22:41:52 +0000

    Agrego metpodos para recuperar y registrar equipos de un grupo

--- quegolazo-code/AccesoADatos/DAOGrupo.cs
+++ quegolazo-code/AccesoADatos/DAOGrupo.cs
@@ -41,7 +41,44 @@ namespace AccesoADatos
                     cmd.Parameters.AddWithValue("@nombre", g.nombre);
                     cmd.CommandText = sql;
                     cmd.ExecuteNonQuery();
+                   
                 }
+
+                registrarEquiposPorGrupo(fase, con, trans);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo registrar el grupo:" + ex.Message);
+            }
+        }
+
+        private void registrarEquiposPorGrupo(Fase fase, SqlConnection con, SqlTransaction trans)
+        {
+            SqlCommand cmd = new SqlCommand();
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.Transaction = trans;
+                foreach (Grupo g in fase.grupos)
+                {
+                    string sql = @"INSERT INTO EquiposXGrupo (idEquipo,idGrupo,idFase,idEdicion)
+                                VALUES (@idEquipo,@idGrupo,@idFase,@idEdicion) 
+                                SELECT SCOPE_IDENTITY()";
+                    foreach(Equipo e in g.equipos)
+                    {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@idEquipo", e.idEquipo);
+                    cmd.Parameters.AddWithValue("@idGrupo", g.idGrupo);
+                    cmd.Parameters.AddWithValue("@idFase", fase.idFase);
+                    cmd.Parameters.AddWithValue("@idEdicion", fase.idEdicion);
+                    cmd.CommandText = sql;
+                    cmd.ExecuteNonQuery();
+                    }
+                }
+
+
             }
             catch (Exception ex)
             {
@@ -62,6 +99,7 @@ namespace AccesoADatos
                             string sql = @"SELECT * 
                                             FROM Grupos
                                             WHERE idFase=@idFase AND idEdicion = @idEdicion";
+                            cmd.Parameters.Clear();            
                             cmd.Parameters.AddWithValue("@idFase", fase.idFase);
                             cmd.Parameters.AddWithValue("@idEdicion", fase.idEdicion);
                             cmd.CommandText = sql;
@@ -73,12 +111,14 @@ namespace AccesoADatos
                                     idGrupo = int.Parse(dr["idGrupo"].ToString()),
                                     idEdicion=fase.idEdicion,
                                     idFase=fase.idFase,
-                                    nombre= int.Parse(dr["nombre"].ToString()),                                 
+                                    nombre= int.Parse(dr["nombre"].ToString()),
+                                    //equipos = obtenerEquipos(con, trans, int.Parse(dr["idGrupo"].ToString()), fase.idEdicion, fase.idFase)
                                 };
-                                fase.grupos.Add(grupo);
+                                fase.grupos.Add(grupo);           
                             }
                             if (dr != null)
                                 dr.Close();
+                            obtenerEquipos(con, trans, fase);
             }
             catch (Exception ex)
             {
@@ -90,5 +130,55 @@ namespace AccesoADatos
                     con.Close();
             }
         }
+
+         public void obtenerEquipos(SqlConnection con, SqlTransaction trans, Fase fase)
+        {
+           
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader dr;
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.Transaction = trans;
+                foreach (Grupo g in fase.grupos)
+                {
+
+                    string sql = @"SELECT eg.idEquipo, e.nombre
+                                        FROM EquiposXGrupo eg join Equipos  e on eg.idEquipo=e.idEquipo 
+                                        WHERE eg.idFase=@idFase AND eg.idEdicion=@idEdicion AND eg.idGrupo=@idGrupo";
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@idFase", fase.idFase);
+                    cmd.Parameters.AddWithValue("@idEdicion", fase.idEdicion);
+                    cmd.Parameters.AddWithValue("@idGrupo", g.idGrupo);
+                    cmd.CommandText = sql;
+                    dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Equipo equipo = new Equipo()
+                        {
+                            idEquipo = int.Parse(dr["idEquipo"].ToString()),
+                            nombre = dr["nombre"].ToString(),
+                        };
+                        g.equipos.Add(equipo);
+                    }
+                    if (dr != null)
+                        dr.Close();
+                }
+                            
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo obtener los datos del Equipo" + ex.Message);
+            }
+            finally
+            {
+                if (con != null && con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+
     }
 }
