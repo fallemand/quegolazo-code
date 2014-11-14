ad20259173c72b0e4f5ace19389b5410bd630023
antonioherrera990 <antonioherrera990@gmail.com>
2014-11-14 19:10:44 +0000

    Argego cambio en constructor

--- quegolazo-code/Entidades/TipoFixture.cs
+++ quegolazo-code/Entidades/TipoFixture.cs
@@ -10,5 +10,40 @@ namespace Entidades
     {
         public string idTipoFixture { get; set; }
         public string nombre { get; set; }
+        
+        public TipoFixture(string id)
+        {
+            idTipoFixture = id;
+            nombre= obtenerNombre(id);
+        }
+        public TipoFixture()
+        {
+
+        }
+        /// <summary>
+        /// Devuelve el nombre de un tipo de fixture basandose en su id
+        /// </summary>
+        private string obtenerNombre(string id)
+{
+    string nombre = "";
+    switch (id)
+    {
+        case "TCT":
+            nombre = "Todos contra todos";
+            break;
+        case "TCT-IV":
+            nombre = "Todos contra todos, ida y vuelta";
+            break;
+        case "ELIM":
+            nombre = "Eliminatorio";
+            break;
+        case "ELIM-IV":
+            nombre = "Eliminatorio, ida y vuelta";
+            break;
+    }
+    return nombre;
+}
+
+      
     }
 }
