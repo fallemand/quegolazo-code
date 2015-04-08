60c7c8661dddd407dd5533c70502f2685fcb2f05
antonioherrera990 <antonioherrera990@gmail.com>
2015-04-08 20:29:24 +0000

    url rewrite implementation, add index.aspx fomr in tournament folder.

---
+++ quegolazo-code/quegolazo-code/torneo/index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Entidades;
+using Logica;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace quegolazo_code.torneo
+{
+    public partial class Formulario_web11 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Torneo t = new GestorTorneo().obtenerTorneoPorNick(Request["nick"]);
+            String nombre = t.nombre;
+        }
+    }
+}
