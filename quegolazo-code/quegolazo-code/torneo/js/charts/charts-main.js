0f1a3e6ce60a3959823c4e8c0874c6207fbbffb8
facualle <facualle@gmail.com>
2015-04-09 22:39:36 +0000

    -Avances en Interfaz equipo.html

---
+++ quegolazo-code/quegolazo-code/torneo/js/charts/charts-main.js
@@ -0,0 +1,102 @@
+// THEME OPTIONS.JS
+//--------------------------------------------------------------------------------------------------------------------------------
+//This is JS file that contains principal fuctions of theme */
+// -------------------------------------------------------------------------------------------------------------------------------
+// Template Name: Sports Cup- Responsive HTML5  soccer and sports Template.
+// Author: Iwthemes.
+// Name File: main.js
+// Version 1.0 - Created on 20 May 2014
+// Website: http://www.iwthemes.com 
+// Email: support@iwthemes.com
+// Copyright: (C) 2014
+
+$(document).ready(function($) {
+    
+    //=================================== Bar Chart ===================================//
+    // Get context with jQuery - using jQuery's .get() method.
+    //Chart.defaults.global.responsive = true;
+    var ctx = $('#barChart').get(0).getContext("2d");
+    var data = {
+        labels: ["P. Ganados", "P. Empatados", "P. Perdidos"],
+        datasets: [
+            {
+                label: "Partidos Ganados",
+                fillColor: "rgba(151,187,205,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [12,4,3]
+            }
+        ]
+    };
+    
+    // This will get the first returned node in the jQuery collection.
+    var myBarChart = new Chart(ctx).Bar(data);
+    myBarChart.datasets[0].bars[0].fillColor = "rgb(175, 215, 175)"; //bar 1
+    myBarChart.datasets[0].bars[0].strokeColor = "rgb(65, 147, 65)"; //bar 1
+    myBarChart.datasets[0].bars[1].fillColor = "rgb(255, 238, 152)"; //bar 2
+    myBarChart.datasets[0].bars[1].strokeColor = "rgb(231, 179, 84)"; //bar 2
+    myBarChart.datasets[0].bars[2].fillColor = "rgb(231, 184, 184)"; //bar 3
+    myBarChart.datasets[0].bars[2].strokeColor = "rgb(199, 45, 45)"; //bar 3
+    myBarChart.update();
+    
+    
+    //=================================== Pie Chart ===================================//
+    var ctx = $("#pieChart").get(0).getContext("2d");
+    var data = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        }
+    ];
+    
+    var myPieChart = new Chart(ctx).Doughnut(data, {
+        animateScale: true
+    });
+    
+    //=================================== Line Chart ===================================//
+    var ctx = $("#lineChart").get(0).getContext("2d");
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
+    };
+    
+    var myLineChart = new Chart(ctx).Line(data, {
+        bezierCurve: false
+    });
+});	
