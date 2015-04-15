b3b0eb39dd7c8f19dad73764011ad36bbb8ed6b3
antonioherrera990@gmail.com <antonioherrera990@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2015-04-15 21:02:29 +0000

    agrego fixture del torneo (soy Flor)

---
+++ trunk/quegolazo-code/quegolazo-code/torneo/fixture.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Logica;
+
+namespace quegolazo_code.torneo
+{
+    public partial class fechas : System.Web.UI.Page
+    {
+       protected  GestorPartido gestorPartido;
+       public static GestorEdicion gestorEdicion;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gestorPartido = Sesion.getGestorPartido();
+        }
+    }
+}
---
+++ trunk/quegolazo-code/quegolazo-code/torneo/fixture.aspx.designer.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace quegolazo_code.torneo {
+    
+    
+    public partial class fixture {
+        
+        /// <summary>
+        /// Control rptFases.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptFases;
+        
+        /// <summary>
+        /// Control panelSinFases.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelSinFases;
+    }
+}
