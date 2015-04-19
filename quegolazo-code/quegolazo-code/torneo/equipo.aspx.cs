9030250c6e38c1ff033086d50953d36d59cb0270
paupedrosa30 <paupedrosa30@gmail.com>
2015-04-19 02:27:40 +0000

    Avances en equipos

--- quegolazo-code/quegolazo-code/torneo/equipo.aspx.cs
+++ quegolazo-code/quegolazo-code/torneo/equipo.aspx.cs
@@ -4,14 +4,84 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Logica;
+using Utils;
+using Entidades;
 
 namespace quegolazo_code.torneo
 {
     public partial class equipo : System.Web.UI.Page
     {
+        protected GestorTorneo gestorTorneo;
+        protected GestorEdicion gestorEdicion;
+        protected GestorEquipo gestorEquipo;
+        protected GestorPartido gestorPartido;
+        GestorEstadisticas gestorEstadistica;
+        protected bool tieneLogo;
+        protected int idEquipo;
         protected void Page_Load(object sender, EventArgs e)
         {
+            gestorTorneo = Sesion.getGestorTorneo();
+            gestorEdicion = Sesion.getGestorEdicion();
+            //TODO esto está harcodeado para que funque! 2008
+            gestorEdicion.edicion = new GestorEdicion().obtenerEdicionPorId(4007);
+            gestorTorneo.torneo = new GestorTorneo().obtenerTorneoPorId(87);
+            gestorEquipo = Sesion.getGestorEquipo();
+            gestorPartido = Sesion.getGestorPartido();
+            gestorEstadistica = new GestorEstadisticas(); 
+            if (!Page.IsPostBack)
+            {
+                gestorEquipo.equipo = gestorEquipo.obtenerEquipoPorId(int.Parse(Request["idEquipo"]));
+                cargarDatosPrincipalesEquipo();
+                cargarHistorialDePartidos();
+                cargarGoleadores();
+                cargarRepeaterOtrosEquiposDeEdicion();
+                cargarRepeaterJugadores();
+            }
+        }
 
+        public void cargarDatosPrincipalesEquipo()
+        {
+            var datosPrincipalesEquipo = gestorEstadistica.obtenerEstadisticasEquipo(gestorEquipo.equipo.idEquipo);
+            //Datos Equipo
+            ltPuntos.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["Puntos"].ToString() : "-";
+            ltGolesAFavor.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["GF"].ToString() : "-";
+            ltGolesEnContra.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["GC"].ToString() : "-";
+            ltPartidosJugados.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["PJ"].ToString() : "-";
+            //Datos Resumen
+            ltResumenPartidosJugados.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["PJ"].ToString() : "-";
+            ltResumenPartidosGanados.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["PG"].ToString() : "-";
+            ltResumenPartidosEmpatados.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["PE"].ToString() : "-";
+            ltResumenPartidosPerdidos.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["PP"].ToString() : "-";
+            ltResumenGoles.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? (int.Parse(datosPrincipalesEquipo.Rows[0]["GF"].ToString()) - int.Parse(datosPrincipalesEquipo.Rows[0]["GC"].ToString())).ToString() : "-";
+            ltResumenGolesConvertidos.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["GF"].ToString() : "-";
+            ltResumenGolesEnContra.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["GC"].ToString() : "-";
+            ltResumenTarjetas.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? (int.Parse(datosPrincipalesEquipo.Rows[0]["TA"].ToString()) + int.Parse(datosPrincipalesEquipo.Rows[0]["TR"].ToString())).ToString() : "-";
+            ltResumenTarjetasAmarillas.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["TA"].ToString() : "-";
+            ltResumenTarjetasRojas.Text = (datosPrincipalesEquipo.Rows.Count > 0) ? datosPrincipalesEquipo.Rows[0]["TR"].ToString() : "-";
+        }
+
+        public void cargarHistorialDePartidos()
+        {
+            GestorControles.cargarRepeaterList(rptHistorialPartidos, gestorPartido.ultimosPartidosDeUnEquipo(gestorEquipo.equipo.idEquipo, gestorEdicion.edicion.idEdicion));
+            //GestorControles.cargarRepeaterTable(rptHistorialPartidos, gestorEstadistica.obtenerUltimosPartidosEquipo(gestorEquipo.equipo.idEquipo));
+        }
+
+        public void cargarGoleadores()
+        {
+            GestorControles.cargarRepeaterList(rptGoleadores, gestorEquipo.goleadoresDeUnEquipo(gestorEquipo.equipo.idEquipo, gestorEdicion.edicion.idEdicion));
+        }
+
+
+        public void cargarRepeaterOtrosEquiposDeEdicion()
+        {
+            GestorControles.cargarRepeaterList(rptOtroseEquiposDeEdicion, gestorEdicion.obtenerEquipos());
+        }
+
+        public void cargarRepeaterJugadores()
+        {
+            List<Jugador> jugadores = gestorEquipo.jugadoresDeUnEquipo(gestorEdicion.edicion.idEdicion, gestorEquipo.equipo.idEquipo);
+            GestorControles.cargarRepeaterList(rptJugadoresDelEquipo, gestorEquipo.jugadoresDeUnEquipo(gestorEdicion.edicion.idEdicion, gestorEquipo.equipo.idEquipo));
         }
     }
 }
--- quegolazo-code/quegolazo-code/torneo/equipo.aspx.designer.cs
+++ quegolazo-code/quegolazo-code/torneo/equipo.aspx.designer.cs
@@ -11,5 +11,167 @@ namespace quegolazo_code.torneo {
     
     
     public partial class equipo {
+        
+        /// <summary>
+        /// Control rptOtroseEquiposDeEdicion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptOtroseEquiposDeEdicion;
+        
+        /// <summary>
+        /// Control ltPuntos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltPuntos;
+        
+        /// <summary>
+        /// Control ltGolesAFavor.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltGolesAFavor;
+        
+        /// <summary>
+        /// Control ltGolesEnContra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltGolesEnContra;
+        
+        /// <summary>
+        /// Control ltPartidosJugados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltPartidosJugados;
+        
+        /// <summary>
+        /// Control rptJugadoresDelEquipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptJugadoresDelEquipo;
+        
+        /// <summary>
+        /// Control ltResumenPartidosJugados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenPartidosJugados;
+        
+        /// <summary>
+        /// Control ltResumenPartidosGanados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenPartidosGanados;
+        
+        /// <summary>
+        /// Control ltResumenPartidosEmpatados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenPartidosEmpatados;
+        
+        /// <summary>
+        /// Control ltResumenPartidosPerdidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenPartidosPerdidos;
+        
+        /// <summary>
+        /// Control ltResumenGoles.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenGoles;
+        
+        /// <summary>
+        /// Control ltResumenGolesConvertidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenGolesConvertidos;
+        
+        /// <summary>
+        /// Control ltResumenGolesEnContra.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenGolesEnContra;
+        
+        /// <summary>
+        /// Control ltResumenTarjetas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenTarjetas;
+        
+        /// <summary>
+        /// Control ltResumenTarjetasAmarillas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenTarjetasAmarillas;
+        
+        /// <summary>
+        /// Control ltResumenTarjetasRojas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal ltResumenTarjetasRojas;
+        
+        /// <summary>
+        /// Control rptHistorialPartidos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptHistorialPartidos;
+        
+        /// <summary>
+        /// Control rptGoleadores.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptGoleadores;
     }
 }
