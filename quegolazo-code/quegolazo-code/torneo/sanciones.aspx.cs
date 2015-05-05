9c1908b4d8d481e84002427cefebe329e83b043d
florenciarojasamaya <florenciarojasamaya@gmail.com>
2015-05-05 22:24:04 +0000

    Subo form de sanciones para dejar lindo

---
+++ quegolazo-code/quegolazo-code/torneo/sanciones.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Utils;
+using Logica;
+
+namespace quegolazo_code.torneo
+{
+    public partial class sanciones : System.Web.UI.Page
+    {
+       protected GestorEdicion gestorEdicion;
+       protected GestorTorneo gestorTorneo;
+
+        protected int idEdicion;
+        protected string nickTorneo;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            idEdicion = int.Parse(Request["idEdicion"]);
+            nickTorneo = Request["nickTorneo"];
+            gestorEdicion = Sesion.getGestorEdicion();
+            gestorTorneo = Sesion.getGestorTorneo();
+            
+
+            if (!Page.IsPostBack)
+            {
+                gestorTorneo.torneo = new GestorTorneo().obtenerTorneoPorNick(nickTorneo);
+                obtenerEdicionSeleccionada();
+                cargarRepeaterSanciones(idEdicion);
+                cargarRepeaterTarjetas();
+            }
+    
+        }
+
+        private void cargarRepeaterSanciones(int idEdicion)
+        {
+            GestorSancion gestorSancion = new GestorSancion();
+            sinSanciones.Visible = !GestorControles.cargarRepeaterTable(rptSanciones, gestorSancion.obtenerSancionesDeUnaEdicion(idEdicion.ToString()));
+        }
+
+        private void cargarRepeaterTarjetas()
+        {
+            GestorEstadisticas gestorEstadisticas = new GestorEstadisticas();
+            sinTarjetas.Visible = !GestorControles.cargarRepeaterTable(rptTarjetas, gestorEstadisticas.obtenerTablaTarjetas());
+        }
+
+         protected  void obtenerEdicionSeleccionada()
+        {
+            gestorEdicion.edicion = gestorEdicion.obtenerEdicionPorId(idEdicion);
+            gestorEdicion.edicion.fases = gestorEdicion.obtenerFases();
+            gestorEdicion.edicion.equipos = gestorEdicion.obtenerEquipos();
+        }
+
+
+    }
+}
---
+++ quegolazo-code/quegolazo-code/torneo/sanciones.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace quegolazo_code.torneo {
+    
+    
+    public partial class sanciones {
+        
+        /// <summary>
+        /// Control rptTarjetas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptTarjetas;
+        
+        /// <summary>
+        /// Control sinTarjetas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableRow sinTarjetas;
+        
+        /// <summary>
+        /// Control rptSanciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptSanciones;
+        
+        /// <summary>
+        /// Control sinSanciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTableRow sinSanciones;
+    }
+}
