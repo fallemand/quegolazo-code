f8782e543f586b18a6b1f8b52f6ba4505f70a7dc
antonioherrera990@gmail.com <antonioherrera990@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2015-05-07 20:48:02 +0000

    Avance en tabla de posiciones, creoq ue quedo bastante bien, no se que mas le pondria, besos

--- trunk/quegolazo-code/quegolazo-code/torneo/posiciones.aspx.designer.cs
+++ trunk/quegolazo-code/quegolazo-code/torneo/posiciones.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace quegolazo_code.torneo {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptEquipos;
+        
+        /// <summary>
+        /// Control rptListaEquipos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptListaEquipos;
     }
 }
