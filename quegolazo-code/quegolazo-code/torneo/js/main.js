7c38451fe9ba84a6438dc145c31adea6e465f492
facualle <facualle@gmail.com>
2015-05-08 18:41:01 +0000

    -Estilos 404

--- quegolazo-code/quegolazo-code/torneo/js/main.js
+++ quegolazo-code/quegolazo-code/torneo/js/main.js
@@ -236,7 +236,7 @@ $(document).ready(function ($) {
 
     $(".torneos-slide").owlCarousel({
         autoPlay: 3200,
-        items: 8,
+        items: 6,
         navigation: false,
         itemsDesktop: [1199, 5],
         itemsDesktopSmall: [1024, 4],
