f97f0a1745cb54a301fc9f15f8e5b05382861e33
paupedrosa30@gmail.com <paupedrosa30@gmail.com@67143110-746c-5885-1a98-ce200b39046c>
2015-05-19 00:01:27 +0000

    Soluciono bug de miscelanea: valla menos vencida

--- trunk/quegolazo-code/quegolazo-code/torneo/miscelaneas.aspx.cs
+++ trunk/quegolazo-code/quegolazo-code/torneo/miscelaneas.aspx.cs
@@ -51,15 +51,7 @@ namespace quegolazo_code.torneo
             }
             catch (Exception ex) { 
                 GestorError.mostrarPanelFracaso(ex.Message);
-            }
-        
-
+            }  
         }
-
-
-
-
-
-
     }
  }
